dde48392f3c538eaf35a19d0fd763c17ea75c10c
Anurag Rawat <hackprajjwal2025@gmail.com>
2021-06-02 20:30:02 +0530

    initial commit

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -4,7 +4,7 @@ const Header = ()=>{
     return (
         <div className="header">
             <div className="logo">
-                <img src="https://beta.huddle01.com/static/media/logo.cfd383f4.svg"></img>
+                <img alt="logo" src="https://beta.huddle01.com/static/media/logo.cfd383f4.svg"></img>
             </div>
         </div>
     )
